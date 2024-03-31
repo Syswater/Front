33fe79098dcccc5c0e8531ebb0327b6403e0fcd0
Cristian David Parada Martinez <crispo9028@gmail.com>
2024-03-31 11:51:42 -0500

    First Commit: Structure

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MaterialModule { }
