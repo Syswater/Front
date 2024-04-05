d2f7397755e9eb62ca2b093a76c6f28ba4fecb4d
echendi <nidafa202426@gmail.com>
2024-04-05 11:09:41 -0500

    feat: añadir la url de la API a variables de entorno

--- src/data/services/auth.service.ts
+++ src/data/services/auth.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  url = 'https://jb02gq55-3000.use2.devtunnels.ms'
+  url = environment.API_URL
 
   constructor(private http: HttpClient) { }
 
   async login(credentials: any) {
     return await firstValueFrom(this.http.post(`${this.url}/auth/login`, { username: credentials.username, password: credentials.password }));
   }
-  
+
 }
